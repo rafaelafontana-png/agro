8144c0b282d4eeed385ee0305ea4042b8f6a4915
rafaelafontana-png <fontana.rafaela@escola.pr.gov.br>
2026-06-10 15:33:42 -0300

    Add functions to show message and calculate CO2

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+function mostrarMensagem(){
+    alert(
+        "O Agro Forte busca unir produtividade e preservação ambiental para garantir um futuro sustentável."
+    );
+}
+
+function calcularCO2(){
+
+    let arvores = document.getElementById("arvores").value;
+
+    if(arvores <= 0){
+        document.getElementById("resultado").innerHTML =
+        "Digite uma quantidade válida.";
+        return;
+    }
+
+    let co2 = arvores * 21;
+
+    document.getElementById("resultado").innerHTML =
+    `🌳 ${arvores} árvores podem absorver aproximadamente ${co2} kg de CO₂ por ano.`;
+}
